fe96894540a6e11d5b4cb48b9393b7d0e04ac8f8
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2025-04-22 18:46:35 +0100

    fix

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,20 @@
 
     {{-- CSS --}}
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.svg') }}" type="image/x-icon">
-    <link rel="stylesheet" href="{{ asset('assets/css/tailwind.css') }}">
+
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}"
+          type="image/x-icon"
+    />
+    <link rel="stylesheet" href="{{ asset('css/swiper-bundle.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <!-- ==== WOW JS ==== -->
+    <script src="{{ asset('js/wow.min.js') }}"></script>
+    <script>
+        new WOW().init();
+    </script>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- Livewire Styles --}}
     @livewireStyles
@@ -22,5 +35,78 @@
 
     {{-- Livewire Scripts --}}
     @livewireScripts
+
+    <script src="{{ asset('js/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+    <script>
+        // ==== for menu scroll
+        const pageLink = document.querySelectorAll(".ud-menu-scroll");
+
+        pageLink.forEach((elem) => {
+            elem.addEventListener("click", (e) => {
+                e.preventDefault();
+                document.querySelector(elem.getAttribute("href")).scrollIntoView({
+                    behavior: "smooth",
+                    offsetTop: 1 - 60,
+                });
+            });
+        });
+
+        // section menu active
+        function onScroll(event) {
+            const sections = document.querySelectorAll(".ud-menu-scroll");
+            const scrollPos =
+                window.pageYOffset ||
+                document.documentElement.scrollTop ||
+                document.body.scrollTop;
+
+            for (let i = 0; i < sections.length; i++) {
+                const currLink = sections[i];
+                const val = currLink.getAttribute("href");
+                const refElement = document.querySelector(val);
+                const scrollTopMinus = scrollPos + 73;
+                if (
+                    refElement.offsetTop <= scrollTopMinus &&
+                    refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+                ) {
+                    document
+                        .querySelector(".ud-menu-scroll")
+                        .classList.remove("active");
+                    currLink.classList.add("active");
+                } else {
+                    currLink.classList.remove("active");
+                }
+            }
+        }
+
+        window.document.addEventListener("scroll", onScroll);
+
+        // Testimonial
+        const testimonialSwiper = new Swiper(".testimonial-carousel", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+
+            // Navigation arrows
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                },
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+                1280: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+            },
+        });
+    </script>
 </body>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,45 +1,25 @@
-<!doctype html>
-<html lang="en" class="scroll-smooth">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>
-      Play | Free Tailwind CSS Template for Startup and SaaS By TailGrids
-    </title>
-    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}"
-      type="image/x-icon"
-    />
-    <link rel="stylesheet" href="{{ asset('css/swiper-bundle.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/animate.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
-    <!-- ==== WOW JS ==== -->
-    <script src="{{ asset('js/wow.min.js') }}"></script>
-    <script>
-      new WOW().init();
-    </script>
-  </head>
+@extends('layout.app')
 
-  <body>
-    <!-- ====== Navbar Section Start -->
+@section('content')
+<!-- ====== Navbar Section Start -->
     <x-navbar/>
-    
+
     <!-- ====== Navbar Section End -->
 
     <!-- ====== Hero Section Start -->
     <x-hero/>
-    
+
     <!-- ====== Hero Section End -->
-   
-   
+
+
     <!-- ====== Features Section Start -->
-   
-   
+
+
     <x-features/>
     <!-- ====== Features Section End -->
 
     <!-- ====== About Section Start -->
-   
+
     <x-about/>
     <!-- ====== About Section End -->
 
@@ -146,18 +126,18 @@
     <!-- ====== CTA Section End -->
 
     <!-- ====== Pricing Section Start -->
-   
+
     <x-pricing/>
     <!-- ====== Pricing Section End -->
 
     <!-- ====== Testimonial Section Start -->
-   
+
     <x-Testimonials/>
-   
+
     <!-- ====== Testimonial Section End -->
 
     <!-- ====== FAQ Section Start -->
-   
+
     <x-faq/>
 
     <!-- ====== FAQ Section End -->
@@ -1247,12 +1227,12 @@
 
     <!-- ====== Contact Start ====== -->
     <x-contact/>
-  
+
     <!-- ====== Contact End ====== -->
 
     <!-- ====== Brands Section Start -->
     <x-brands/>
-  
+
     <!-- ====== Brands Section End -->
 
     <!-- ====== Footer Section Start -->
@@ -1298,77 +1278,6 @@
 
     <!-- ====== All Scripts -->
 
-    <script src="{{ asset('js/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
-    <script>
-      // ==== for menu scroll
-      const pageLink = document.querySelectorAll(".ud-menu-scroll");
 
-      pageLink.forEach((elem) => {
-        elem.addEventListener("click", (e) => {
-          e.preventDefault();
-          document.querySelector(elem.getAttribute("href")).scrollIntoView({
-            behavior: "smooth",
-            offsetTop: 1 - 60,
-          });
-        });
-      });
 
-      // section menu active
-      function onScroll(event) {
-        const sections = document.querySelectorAll(".ud-menu-scroll");
-        const scrollPos =
-          window.pageYOffset ||
-          document.documentElement.scrollTop ||
-          document.body.scrollTop;
-
-        for (let i = 0; i < sections.length; i++) {
-          const currLink = sections[i];
-          const val = currLink.getAttribute("href");
-          const refElement = document.querySelector(val);
-          const scrollTopMinus = scrollPos + 73;
-          if (
-            refElement.offsetTop <= scrollTopMinus &&
-            refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
-          ) {
-            document
-              .querySelector(".ud-menu-scroll")
-              .classList.remove("active");
-            currLink.classList.add("active");
-          } else {
-            currLink.classList.remove("active");
-          }
-        }
-      }
-
-      window.document.addEventListener("scroll", onScroll);
-
-      // Testimonial
-      const testimonialSwiper = new Swiper(".testimonial-carousel", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-
-        // Navigation arrows
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-
-        breakpoints: {
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-          1280: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-        },
-      });
-    </script>
-  </body>
-</html>
+@endsection
